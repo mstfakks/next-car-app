eb27fc6ba1207de78fe62713e0a32f31af0c81f5
mstfakks <msfa.akkas@gmail.com>
2023-08-14 23:29:47 +0300

    Search Bar completed.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,57 @@
 import { useState } from "react";
 import { SearchManufacturer } from ".";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+  return (
+    <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image
+        src="/magnifying-glass.svg"
+        alt="magnifying glass"
+        width={40}
+        height={40}
+        className="object-contain"
+      />
+    </button>
+  );
+};
 
 export default function SearchBar() {
+  const router = useRouter();
   const [manufacturer, setManufacturer] = useState("");
-  function handleSearch() {
-    console.log("searching...");
-  }
+  const [model, setModel] = useState("");
+
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please enter a manufacturer or model");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathName = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+    router.push(newPathName);
+  };
+
   return (
     <form
       className="flex items-center justify-start max-sm:flex-col w-full relative max-sm:gap-4 max-w-3xl"
@@ -16,7 +62,27 @@ export default function SearchBar() {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="flex-1 max-sm:w-full flex justify-start items-center relative">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(event) => setModel(event.target.value)}
+          placeholder="Tiguan"
+          className="w-full h-[48px] pl-12 p-4 bg-light-white rounded-r-full max-sm:rounded-full outline-none cursor-pointer text-sm"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 }
